3a46a5872a887844afa1ac201a94ef46d986f482
JackOlington <98637298+JackOlington@users.noreply.github.com>
2024-11-09 23:23:03 +0800

    Add files via upload
    camera function 
    lack post and response from server

--- lib/pages/camera_page.dart
+++ lib/pages/camera_page.dart
@@ -1,20 +1,49 @@
-// lib/pages/camera_page.dart
+// lib/pages/history_page.dart
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class CameraPage extends StatelessWidget {
-  const CameraPage({super.key});
 
+  class CamaraPage extends StatefulWidget {
+    const CamaraPage({super.key});
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Camera', style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: const Center(
-        child: Text('Camera Page Content'),
-      ),
-    );
+    State<CamaraPage> createState() => _CamaraPageState();
   }
-}
+
+  class _CamaraPageState extends State<CamaraPage> {
+    final ImagePicker _picker = ImagePicker();
+    XFile? _image;
+
+    Future<void> _getImage() async {
+      // final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+      setState(() {
+        _image = image;
+      });
+    }
+
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+        appBar: AppBar(
+          title: Text('Camara'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _image == null? Text('No image selected.') : Image.file(File(_image!.path)),
+        ElevatedButton(
+        child: Text("Take a shot"),
+        onPressed: _getImage,
+        style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white, backgroundColor: Colors.blue, // Text color
+        ),
+        ),
+        ]
+        )
+        )
+        );
+      }
+  }
